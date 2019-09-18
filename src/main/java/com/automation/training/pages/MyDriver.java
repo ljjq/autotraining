7db48df88f9d4f9a3982f8b3e78b16a34d49e583
Johana Jaramillo <johanajaramillo0227@gmail.com>
2019-09-17 22:47:19 -0500

    Unified code

--- src/main/java/com/automation/training/pages/MyDriver.java
+++ src/main/java/com/automation/training/pages/MyDriver.java
@@ -1,6 +1,7 @@
 package com.automation.training.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -25,7 +26,7 @@ public class MyDriver {
 
             case "chrome":
                 System.setProperty("webdriver.chrome.driver","/Users/user/Documents/chromedriver");
-                driver = new FirefoxDriver();
+                driver = new ChromeDriver();
                 break;
        }
     }
